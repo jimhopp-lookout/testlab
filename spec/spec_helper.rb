a1e71dc59083d5550dee2fb595ac932cf3d8f372
Zachary Patten <zachary@jovelabs.com>
2013-04-13 23:39:20 -0700

    setup specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,42 @@
+################################################################################
+#
+#      Author: Zachary Patten <zachary@jovelabs.net>
+#   Copyright: Copyright (c) Zachary Patten
+#     License: Apache License, Version 2.0
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+################################################################################
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end if ENV["COVERAGE"]
+################################################################################
+require 'testlab'
+
+ENV['LOG_LEVEL'] = "DEBUG"
+
+RSpec.configure do |config|
+
+  config.before(:all) do
+    $stdout = File.open("/dev/null", "w")
+    $stderr = File.open("/dev/null", "w")
+    $stdin = File.open("/dev/null", "r")
+    $logger = ZTK::Logger.new(File.join("/tmp", "test.log"))
+
+    $logger.info { "=" * 80 }
+    $logger.info { "STARTING TESTLAB v#{TestLab::VERSION} TEST RUN @ #{Time.now.utc}" }
+    $logger.info { "=" * 80 }
+  end
+
+end
---
+++ spec/testlab_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe TestLab do
+
+  subject { TestLab.new }
+
+  describe "class" do
+
+    it "should be an instance of TestLab" do
+      subject.should be_an_instance_of TestLab
+    end
+
+  end
+
+end
