636b5733e6665f96eab7706ec73b878b47dcb365
Zachary Patten <zachary@jovelabs.com>
2013-04-23 18:37:12 -0700

    * move the create method call into our after_up callback * add a placeholder for our before destroy callback

--- lib/testlab/container.rb
+++ lib/testlab/container.rb
@@ -102,14 +102,13 @@ class TestLab
     # Container Callback: after_create
     def after_create
       @ui.logger.debug { "Container Callback: After Create: #{self.id} " }
-
-      self.create
     end
 
     # Container Callback: after_up
     def after_up
       @ui.logger.debug { "Container Callback: After Up: #{self.id} " }
 
+      self.create
       self.up
     end
 
@@ -118,6 +117,12 @@ class TestLab
       @ui.logger.debug { "Container Callback: Before Down: #{self.id} " }
 
       self.down
+      self.destroy
+    end
+
+    # Container Callback: before_destroy
+    def before_destroy
+      @ui.logger.debug { "Container Callback: Before Destroy: #{self.id} " }
     end
 
 ################################################################################
