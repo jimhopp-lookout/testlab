79b1f76c03c41825a8e86b9df1225c78a3689397
Zachary Patten <zachary@jovelabs.com>
2013-04-28 11:33:02 -0700

    add the node id to the status hash

--- lib/testlab/node/status.rb
+++ lib/testlab/node/status.rb
@@ -8,6 +8,7 @@ class TestLab
       # @return [Hash] A hash detailing the status of the node.
       def status
         {
+          :id => self.id,
           :instance_id => @provider.instance_id,
           :state => @provider.state,
           :user => @provider.user,
