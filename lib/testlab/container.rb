3d3ea2ef5f9cb6dc064ef21938ebfd670b6bfa72
Zachary Patten <zachary@jovelabs.com>
2013-04-27 21:16:07 -0700

    add ptr and cidr helper methods to the container object

--- lib/testlab/container.rb
+++ lib/testlab/container.rb
@@ -72,6 +72,28 @@ class TestLab
       self.primary_interface.last[:ip].split('/').first
     end
 
+    # Returns the CIDR of the container
+    def cidr
+      self.primary_interface.last[:ip].split('/').last.to_i
+    end
+
+    def ptr
+      octets = self.ip.split('.')
+
+      result = case self.cidr
+      when 0..7 then
+        octets[-4,4]
+      when 8..15 then
+        octets[-3,3]
+      when 16..23 then
+        octets[-2,2]
+      when 24..31 then
+        octets[-1,1]
+      end
+
+      result.reverse.join('.')
+    end
+
     def primary_interface
       if self.interfaces.any?{ |i,c| c[:primary] == true }
         self.interfaces.find{ |i,c| c[:primary] == true }
