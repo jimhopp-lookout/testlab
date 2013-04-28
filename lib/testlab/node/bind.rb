acb29f0a85220f976957d0eebf744a942fe9e2ee
Zachary Patten <zachary@jovelabs.com>
2013-04-27 21:17:09 -0700

    refactor the bind code

--- lib/testlab/node/bind.rb
+++ lib/testlab/node/bind.rb
@@ -12,9 +12,31 @@ class TestLab
         file.puts(ZTK::Template.render(bind_conf_template, {}))
       end
 
+      def build_bind_records
+        forward_records = Hash.new
+        reverse_records = Hash.new
+
+        TestLab::Container.all.each do |container|
+          interface = container.primary_interface
+          puts interface.inspect
+          tld = (container.tld || container.node.labfile.config[:tld])
+
+          forward_records[tld] ||= Array.new
+          forward_records[tld] << %(#{container.id} IN A #{container.ip})
+
+          reverse_records[interface.first] ||= Array.new
+          reverse_records[interface.first] << %(#{container.ptr} IN PTR #{container.id}.#{tld}.)
+        end
+        { :forward => forward_records, :reverse => reverse_records }
+      end
+
       # Builds the bind configuration sections for our zones
       def build_bind_zone_partial(file)
         bind_zone_template = File.join(self.class.template_dir, 'bind-zone.erb')
+        bind_records = build_bind_records
+        puts("bind_records == #{bind_records.inspect}")
+        forward_records = bind_records[:forward]
+        reverse_records = bind_records[:reverse]
 
         TestLab::Network.all.each do |network|
           context = {
@@ -24,7 +46,7 @@ class TestLab
           file.puts
           file.puts(ZTK::Template.render(bind_zone_template, context))
 
-          build_bind_db(network.arpa)
+          build_bind_db(network.arpa, reverse_records[network.id])
         end
 
         tlds = ([self.labfile.config[:tld]] + TestLab::Container.tlds).flatten
@@ -36,21 +58,21 @@ class TestLab
           file.puts
           file.puts(ZTK::Template.render(bind_zone_template, context))
 
-          build_bind_db(tld)
+          build_bind_db(tld, forward_records[tld])
         end
       end
 
-      def build_bind_db(zone)
+      def build_bind_db(zone, records)
         bind_db_template = File.join(self.class.template_dir, 'bind-db.erb')
 
         tempfile = Tempfile.new("bind-db")
         File.open(tempfile, 'w') do |file|
-          file.puts(ZTK::Template.do_not_edit_notice(:message => "TestLab v#{TestLab::VERSION} BIND DB File", :char => ';'))
-          file.puts(ZTK::Template.render(bind_db_template, {}))
+          file.puts(ZTK::Template.do_not_edit_notice(:message => "TestLab v#{TestLab::VERSION} BIND DB: #{zone}", :char => ';'))
+          file.puts(ZTK::Template.render(bind_db_template, { :zone => zone, :records => records }))
         end
         self.ssh.upload(tempfile.path, File.basename(tempfile.path))
-        self.ssh.exec(%(sudo mv -v #{File.basename(tempfile.path)} /var/lib/bind/db.#{zone}))
-        self.ssh.exec(%(sudo rm -fv /var/lib/bind/db.#{zone}.jnl))
+        self.ssh.exec(%(sudo mv -v #{File.basename(tempfile.path)} /etc/bind/db.#{zone}))
+        self.ssh.exec(%(sudo rm -fv /etc/bind/db.#{zone}.jnl))
       end
 
       # Builds the BIND configuration
@@ -66,8 +88,6 @@ class TestLab
 
         self.ssh.upload(tempfile.path, File.basename(tempfile.path))
         self.ssh.exec(%(sudo mv -v #{File.basename(tempfile.path)} #{bind_conf}))
-
-        self.ssh.exec(%(sudo /bin/bash -c 'service bind9 restart || service bind9 start'))
       end
 
       def bind_setup
@@ -75,6 +95,8 @@ class TestLab
         self.ssh.bootstrap(ZTK::Template.render(bind_setup_template))
 
         build_bind_conf
+
+        self.ssh.exec(%(sudo /bin/bash -c 'service bind9 restart || service bind9 start'))
       end
 
     end
