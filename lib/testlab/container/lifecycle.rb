1702ce2b75371a827387c8165616e345b835aab7
Zachary Patten <zachary@jovelabs.com>
2013-04-28 18:55:16 -0700

    call the provisioner during setup and teardown

--- lib/testlab/container/lifecycle.rb
+++ lib/testlab/container/lifecycle.rb
@@ -9,11 +9,19 @@ class TestLab
 
         self.create
         self.up
+
+        if (!@provisioner.nil? && @provisioner.respond_to?(:setup))
+          @provisioner.setup(self)
+        end
       end
 
       # Container Teardown
       def teardown
         @ui.logger.debug { "Container Teardown: #{self.id} " }
+
+        if (!@provisioner.nil? && @provisioner.respond_to?(:teardown))
+          @provisioner.teardown(self)
+        end
 
         self.down
         self.destroy
