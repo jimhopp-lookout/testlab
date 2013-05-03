3ce4bfe7c4d32649345dab1c1d8e0d1e51ace756
Zachary Patten <zachary@jovelabs.com>
2013-05-03 11:57:03 -0700

    refactor some status/state methods to the proper module; updated documentation

--- lib/testlab/container/interface.rb
+++ lib/testlab/container/interface.rb
@@ -3,21 +3,6 @@ class TestLab
 
     module Interface
 
-      # Returns the IP of the container
-      def ip
-        TestLab::Utility.ip(self.primary_interface.address)
-      end
-
-      # Returns the CIDR of the container
-      def cidr
-        TestLab::Utility.cidr(self.primary_interface.address).to_i
-      end
-
-      # Returns a BIND PTR record
-      def ptr
-        TestLab::Utility.ptr(self.primary_interface.address)
-      end
-
       # Returns the primary interface for the container
       def primary_interface
         if self.interfaces.any?{ |i| i.primary == true }
--- lib/testlab/container/status.rb
+++ lib/testlab/container/status.rb
@@ -3,12 +3,39 @@ class TestLab
 
     module Status
 
+      # Container IP
+      #
+      # Returns the IP of the container
+      def ip
+        TestLab::Utility.ip(self.primary_interface.address)
+      end
+
+      # Container CIDR
+      #
+      # Returns the CIDR of the container
+      def cidr
+        TestLab::Utility.cidr(self.primary_interface.address).to_i
+      end
+
+      # Container BIND PTR Record
+      #
+      # Returns a BIND PTR record
+      def ptr
+        TestLab::Utility.ptr(self.primary_interface.address)
+      end
+
+      # Container FQDN
+      #
+      # Returns the FQDN for the container
       def fqdn
         self.domain ||= self.node.labfile.config[:domain]
 
         [self.id, self.domain].join('.')
       end
 
+      # Container Status
+      #
+      # Returns a hash of status information for the container
       def status
         interfaces = self.interfaces.collect do |interface|
           "#{interface.network_id}:#{interface.name}:#{interface.ip}/#{interface.cidr}"
@@ -26,7 +53,9 @@ class TestLab
         }
       end
 
-      # State of the container
+      # Container State
+      #
+      # What state the container is in.
       def state
         self.lxc.state
       end
