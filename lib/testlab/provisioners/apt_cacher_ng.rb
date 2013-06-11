f4979056426e37c07a9f2d75fbc242edfdf72856
Zachary Patten <zachary@jovelabs.com>
2013-06-10 19:35:46 -0700

    cleanup

--- lib/testlab/provisioners/apt_cacher_ng.rb
+++ lib/testlab/provisioners/apt_cacher_ng.rb
@@ -2,10 +2,10 @@ class TestLab
 
   class Provisioner
 
-    # AptcAcherNG Provisioner Error Class
+    # AptCacherNG Provisioner Error Class
     class AptCacherNGError < ProvisionerError; end
 
-    # AptcAcherNG Provisioner Class
+    # AptCacherNG Provisioner Class
     #
     # @author Zachary Patten <zachary AT jovelabs DOT com>
     class AptCacherNG
@@ -17,18 +17,20 @@ class TestLab
         @ui.logger.debug { "config(#{@config.inspect})" }
       end
 
-      # AptcAcherNG Provisioner Container Setup
+      # AptCacherNG Provisioner Container Setup
       #
       # @param [TestLab::Container] container The container which we want to
       #   provision.
       # @return [Boolean] True if successful.
       def setup(container)
         # Ensure the container APT calls use apt-cacher-ng on the node
-        gateway_ip = container.primary_interface.network.ip
+        gateway_ip            = container.primary_interface.network.ip
         apt_conf_d_proxy_file = File.join(container.lxc.fs_root, "etc", "apt", "apt.conf.d", "00proxy")
+
         container.node.ssh.exec(%(sudo mkdir -pv #{File.dirname(apt_conf_d_proxy_file)}))
+
         container.node.ssh.exec(%(echo 'Acquire::HTTP { Proxy "http://#{gateway_ip}:3142"; };' | sudo tee #{apt_conf_d_proxy_file}))
-        container.config.has_key?(:apt_cacher_exclude_hosts) and container.config[:apt_cacher_exclude_hosts].each do |host|
+        container.config[:apt_cacher_exclude_hosts].each do |host|
           container.node.ssh.exec(%(echo 'Acquire::HTTP::Proxy::#{host} "DIRECT";' | sudo tee -a #{apt_conf_d_proxy_file}))
         end
 
@@ -37,7 +39,7 @@ class TestLab
         container.node.ssh.exec(%(sudo sed -i 's/127.0.0.1:3142\\///g' #{apt_conf_sources_file}))
       end
 
-      # AptcAcherNG Provisioner Container Teardown
+      # AptCacherNG Provisioner Container Teardown
       #
       # This is a NO-OP currently.
       #
