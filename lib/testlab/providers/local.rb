f732826d871cba3238bd982de3c438e9c1f828c8
Zachary Patten <zachary@jovelabs.com>
2013-06-12 12:04:21 -0700

    cleanup the local provider template

--- lib/testlab/providers/local.rb
+++ lib/testlab/providers/local.rb
@@ -10,9 +10,95 @@ class TestLab
     # @author Zachary Patten <zachary AT jovelabs DOT com>
     class Local
 
-      def initialize(ui=ZTK::UI.new)
-        @ui = ui
+################################################################################
+
+      def initialize(config={}, ui=nil)
+        @config = (config || Hash.new)
+        @ui     = (ui     || TestLab.ui)
+
+        # ensure our local key is there
+        @config[:local] ||= Hash.new
       end
+
+################################################################################
+
+      # This is a NO-OP
+      def create
+        true
+      end
+
+      # This is a NO-OP
+      def destroy
+        true
+      end
+
+################################################################################
+
+      # This is a NO-OP
+      def up
+        self.state
+      end
+
+      # This is a NO-OP
+      def down
+        self.state
+      end
+
+################################################################################
+
+      # This is a NO-OP
+      def reload
+        self.down
+        self.up
+
+        self.state
+      end
+
+################################################################################
+
+      # Inquire the state of the Local-controlled VM
+      def state
+        :running
+      end
+
+################################################################################
+
+      # Does the Local-controlled VM exist?
+      def exists?
+        true
+      end
+
+      # Is the Local-controlled VM alive?
+      def alive?
+        true
+      end
+
+      # Is the Local-controlled VM dead?
+      def dead?
+        false
+      end
+
+      def instance_id
+        %x(hostname).strip
+      end
+
+      def user
+        ENV['USER']
+      end
+
+      def identity
+        File.join(ENV['HOME'], ".ssh", "id_rsa")
+      end
+
+      def ip
+        "127.0.0.1"
+      end
+
+      def port
+        (@config[:local][:port] || 22)
+      end
+
+################################################################################
 
     end
 
