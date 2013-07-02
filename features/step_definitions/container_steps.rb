c6bef24579556b365204d08f1ec23217936e220f
Zachary Patten <zachary@jovelabs.com>
2013-07-02 14:16:07 -0700

    simplify the Labfile for cukes

--- features/step_definitions/container_steps.rb
+++ features/step_definitions/container_steps.rb
@@ -1,35 +1,33 @@
 When /^I get the containers status with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(status -n chef-server,chef-client))
+  container_cmd(app_name, %W(status -n test-server))
 end
 
 When /^I get the containers ssh-config with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(ssh-config -n chef-server,chef-client))
+  container_cmd(app_name, %W(ssh-config -n test-server))
 end
 
 When /^I up the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(up -n chef-client))
+  container_cmd(app_name, %W(up -n test-server))
 end
 
 When /^I down the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(down -n chef-client))
+  container_cmd(app_name, %W(down -n test-server))
 end
 
 When /^I clone the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(clone -n chef-client))
+  container_cmd(app_name, %W(clone -n test-server))
 end
 
 When /^I build the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(build -n chef-client))
+  container_cmd(app_name, %W(build -n test-server))
 end
 
 When /^I export the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(export -n chef-server --output=/tmp/chef-server.sc))
-  container_cmd(app_name, %W(export -n chef-client --output=/tmp/chef-client.sc))
+  container_cmd(app_name, %W(export -n test-server --output=/tmp/test-server.sc))
 end
 
 When /^I import the containers with "([^"]*)"$/ do |app_name|
-  container_cmd(app_name, %W(import -n chef-server --input=/tmp/chef-server.sc))
-  container_cmd(app_name, %W(import -n chef-client --input=/tmp/chef-client.sc))
+  container_cmd(app_name, %W(import -n test-server --input=/tmp/test-server.sc))
 end
 
 def container_cmd(app_name, *args)
