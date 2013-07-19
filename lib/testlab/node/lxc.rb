0623893ebc78bdd240e55984b43471b1ead0784e
Zachary Patten <zachary@jovelabs.com>
2013-07-19 14:18:31 -0700

    add a bootstrap wrapper method to the node

--- lib/testlab/node/lxc.rb
+++ lib/testlab/node/lxc.rb
@@ -4,6 +4,18 @@ class TestLab
     module LXC
       require 'lxc'
 
+      # Node Bootstrap
+      #
+      # Renders the supplied content into a file on the node and proceeds
+      # to execute it on the node as root.
+      #
+      # @param [String] content The content to render on the node and
+      #   execute.  This is generally a bash script of some sort for example.
+      # @return [String] The output of SSH bootstrap.
+      def bootstrap(content, options={})
+        self.ssh.bootstrap(content, options)
+      end
+
       # Returns the LXC object for this Node
       #
       # This object is used to control containers on the node via it's provider
