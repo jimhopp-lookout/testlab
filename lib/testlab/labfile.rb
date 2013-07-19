1fa954d56fc8ec431b0b94d92e33eac26ab7df6b
Zachary Patten <zachary@jovelabs.com>
2013-07-18 21:58:29 -0700

    nothing to see move along

--- lib/testlab/labfile.rb
+++ lib/testlab/labfile.rb
@@ -7,9 +7,9 @@ class TestLab
   #
   # @author Zachary Patten <zachary AT jovelabs DOT com>
   class Labfile < ZTK::DSL::Base
-    attribute  :testlab
     has_many   :nodes,   :class_name => 'TestLab::Node'
 
+    attribute  :testlab
     attribute  :config,  :default => Hash.new
 
     def config_dir
