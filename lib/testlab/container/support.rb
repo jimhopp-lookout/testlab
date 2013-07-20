edcfbcd4eb99f3aa931c5a5eccb024f555f94b92
Zachary Patten <zachary@jovelabs.com>
2013-07-19 23:47:58 -0700

    break out the support methods into their own module

---
+++ lib/testlab/container/support.rb
@@ -0,0 +1,35 @@
+class TestLab
+  class Container
+
+    module Support
+
+      # Returns arguments for lxc-create based on our distro
+      #
+      # @return [Array<String>] An array of arguments for lxc-create
+      def create_args
+        case self.distro.downcase
+        when "ubuntu" then
+          %W(-f /etc/lxc/#{self.id} -t #{self.distro} -- --release #{self.release} --arch #{self.arch})
+        when "fedora" then
+          %W(-f /etc/lxc/#{self.id} -t #{self.distro} -- --release #{self.release})
+        end
+      end
+
+      # Attempt to detect the architecture of the node.  The value returned is
+      # respective to the container distro.
+      #
+      # @return [String] The arch of the node in the context of the container
+      #   distro
+      def detect_arch
+        case self.distro.downcase
+        when "ubuntu" then
+          ((self.node.arch =~ /x86_64/) ? "amd64" : "i386")
+        when "fedora" then
+          ((self.node.arch =~ /x86_64/) ? "amd64" : "i686")
+        end
+      end
+
+    end
+
+  end
+end
