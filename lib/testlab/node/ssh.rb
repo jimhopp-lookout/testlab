16c3881b497c24ae42ea677f7b3e784965957081
Zachary Patten <zachary@jovelabs.com>
2013-07-19 22:01:34 -0700

    move the node bootstrap from the lxc module to the ssh module

--- lib/testlab/node/ssh.rb
+++ lib/testlab/node/ssh.rb
@@ -3,6 +3,18 @@ class TestLab
 
     module SSH
 
+      # Node Bootstrap
+      #
+      # Renders the supplied content into a file on the node and proceeds
+      # to execute it on the node as root.
+      #
+      # @param [String] content The content to render on the node and
+      #   execute.  This is generally a bash script of some sort for example.
+      # @return [String] The output of SSH bootstrap.
+      def bootstrap(content, options={})
+        self.ssh.bootstrap(content, options)
+      end
+
       # SSH to the Node
       def ssh(options={})
         if (!defined?(@ssh) || @ssh.nil?)
