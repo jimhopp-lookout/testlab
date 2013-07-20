226cc969a1584497efc9ba8df365b6e425628e87
Zachary Patten <zachary@jovelabs.com>
2013-07-19 23:29:40 -0700

    drop the self. references

--- lib/testlab/support/execution.rb
+++ lib/testlab/support/execution.rb
@@ -11,33 +11,33 @@ class TestLab
       # Renders the supplied content into a file over the SSH connection and
       # executes it as the 'root' user.
       def bootstrap(content, options={})
-        self.ssh.bootstrap(content, options)
+        ssh.bootstrap(content, options)
       end
 
       # Execute
       #
       # Executes the supplied command over the SSH connection.
       def exec(command, options={})
-        self.ssh.exec(command, options)
+        ssh.exec(command, options)
       end
 
       # File
       #
       # Renders the supplied file over the SSH connection.
       def file(options={}, &block)
-        self.ssh.file(options, &block)
+        ssh.file(options, &block)
       end
 
       # Uploads
       #
       # Uploads the supplied file over the SSH connection.
       def upload(local, remote, options={})
-        self.ssh.upload(local, remote, options)
+        ssh.upload(local, remote, options)
       end
 
       # Download
       def download(remote, local, options={})
-        self.ssh.download(remote, local, options)
+        ssh.download(remote, local, options)
       end
 
     end
